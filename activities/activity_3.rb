527e7003dc4c2af276cea27eeee340d47803ef1e
justced01 <jctdelacarcel@gmail.com>
2022-06-24 15:29:39 +0800

    activity 3: Find the difference of 1st and 2nd, 2nd and 3rd, consecutively

--- activities/activity_3.rb
+++ activities/activity_3.rb
@@ -2,3 +2,18 @@
 #  Write a function that takes an array of ages.
 #  The function will return an array of age differences between the 1st and 2nd age, 2nd and 3rd  .. so on and so forth.
 #  Print on console the result.
+
+def diff_age(arr)
+  count = 0
+  arr.each do |val|
+    count += 1
+    if arr[count] != nil
+      print "Age: #{val} - #{arr[count]}, Diff: #{val - arr[count]}\n"
+    else
+      print "Age: #{val} - 0, Diff: #{val - 0}\n"
+    end
+  end
+end
+
+arr = [5, 25, 55, 2]
+diff_age(arr)
