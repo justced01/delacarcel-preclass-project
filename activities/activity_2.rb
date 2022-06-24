1e1300e3a1b861f696c15cacfdf63b6c9557b6fd
justced01 <jctdelacarcel@gmail.com>
2022-06-24 15:03:23 +0800

    activity 2: Takes an argument then print the fullname

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,10 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+def full_name(first_name, middle_name, last_name)
+  full_name = { first: first_name, middle: middle_name, last: last_name }
+  return "Your fullname: #{full_name[:first]} #{full_name[:middle]} #{full_name[:last]}"
+end
+
+p full_name("Jane", "Smith", "Doe")
