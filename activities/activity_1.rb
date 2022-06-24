13949ae70089f23065158d1a90b5e6ef3e84a0d5
justced01 <jctdelacarcel@gmail.com>
2022-06-24 14:58:19 +0800

    activity 1: calculate average of ages of adult

--- activities/activity_1.rb
+++ activities/activity_1.rb
@@ -2,3 +2,19 @@
 #  Write a function that takes an Array of ages,
 #  the function will return the average age of adults.
 #  Print on console the result.
+
+def calc_ave_age(arr)
+  sum = 0
+  count = 0
+  arr.each do |age|
+    if age >= 18
+      sum += age
+      count += 1
+    end
+  end
+  ave = sum / count
+  return ave
+end
+
+arr = [22, 13, 54, 35, 2]
+p calc_ave_age(arr)
